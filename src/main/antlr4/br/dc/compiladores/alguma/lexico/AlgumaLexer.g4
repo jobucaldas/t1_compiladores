a8a70860dead41dfbbdf6fcf448238f11f9c8ffa
João Bueno <joaovictorbuenodecaldas@gmail.com>
2023-05-29 19:06:23 -0300

    Atualizado casos antlr

--- src/main/antlr4/br/dc/compiladores/alguma/lexico/AlgumaLexer.g4
+++ src/main/antlr4/br/dc/compiladores/alguma/lexico/AlgumaLexer.g4
@@ -1,14 +1,19 @@
 lexer grammar AlgumaLexer;
 
-PALAVRA_CHAVE 
-	:	'DECLARACOES' | 'ALGORITMO' | 'INTEIRO' | 'REAL' | 'ATRIBUIR' | 'A' | 'LER' | 'IMPRIMIR' | 'SE' | 'ENTAO' 
-	| 'ENQUANTO' | 'INICIO' | 'FIM' | 'E' | 'OU' 
-	; 
-
 fragment Letra : 'a'..'z' | 'A'..'Z';
 fragment Digito : '0'..'9';
 
-COMENTARIO : '%' ~('\n' | '\r')* {skip();} ;
+PALAVRA_CHAVE :	'tipo' | 'declare' | 'algoritmo' | 'fim_algoritmo' | 'registro' | 'fim_registro' | 'se' | 'senao' | 'fim_se' | ':' | ',' | '.' | '(' | ')'; 
+TYPES	:	'real' | 'literal' ;
+OP_REL	:	'>' | '>=' | '<' | '<=' | '<>' | '=' ;
+OP_ARIT	:	'+' | '-' | '*' | '/' ;
+
+COMENTARIO : '{' ~('}')* '}' {skip();} ;
+WS : ( ' ' | '\t' | '\r' | '\n' ) {skip();} ;
+
 NUMINT	: ('+'|'-')?('0'..'9')+ ;
 NUMREAL	: ('+'|'-')?('0'..'9')+ ('.' ('0'..'9')+)? ;
 VARIAVEL : ('a'..'z'|'A'..'Z') ('a'..'z'|'A'..'Z'|'0'..'9')* ;
+
+EX_STRING : '"' ~('\n')* ;
+CADEIA : '"' ~('"')* '"' ;
--- src/main/java/br/dc/compiladores/alguma/lexico/Principal.java
+++ src/main/java/br/dc/compiladores/alguma/lexico/Principal.java
@@ -17,15 +17,25 @@ public class Principal {
             AlgumaLexer lex = new AlgumaLexer(cs);
 
             Token t = null;
+            // Itera todos os tokens
             while ((t = lex.nextToken()).getType() != Token.EOF) {
-                pw.println("<"+ AlgumaLexer.VOCABULARY.getDisplayName(t.getType()) + "," + t.getText()+">");
+                if((AlgumaLexer.VOCABULARY.getDisplayName(t.getType()) == "OP_REL")
+                 | (AlgumaLexer.VOCABULARY.getDisplayName(t.getType()) == "OP_ARIT")
+                 | (AlgumaLexer.VOCABULARY.getDisplayName(t.getType()) == "PALAVRA_CHAVE")
+                 | (AlgumaLexer.VOCABULARY.getDisplayName(t.getType()) == "TYPES"))
+                {
+                    pw.println("<'" + t.getText() + "','" + t.getText() + "'>");
+                } else{
+                    pw.println("<'" + t.getText() + "'," + AlgumaLexer.VOCABULARY.getDisplayName(t.getType()) + ">");
+                }
             }
 
             // while (lex.nextToken().getType() != Token.EOF) {
             //     pw.println("");
             // }
         } catch(IOException ex) {
-            System.err.println("O arquivo/diretório não existe:"+args[1]);
+            System.err.println("Problema ao abrir o arquivo '" + args[1] + "'");
+            System.err.println("Erro: " + ex.getLocalizedMessage());
         }
     }
 }
